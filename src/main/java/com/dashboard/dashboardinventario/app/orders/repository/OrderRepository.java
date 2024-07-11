6b747014f6b3eaa459f135e847d1cb7a53a92ed9
Lizandro <lizandrojesus13@hotmail.com>
2024-07-11 19:34:19 +0200

    Creando query para consultar los pedidos de cada cliente

--- src/main/java/com/dashboard/dashboardinventario/app/orders/repository/OrderRepository.java
+++ src/main/java/com/dashboard/dashboardinventario/app/orders/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.dashboard.dashboardinventario.app.orders.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
+import com.dashboard.dashboardinventario.app.clients.models.entities.ClientEntity;
 import com.dashboard.dashboardinventario.app.orders.models.entities.OrderEntity;
 
-public interface OrderRepository extends CrudRepository<OrderEntity,Integer> {
+public interface OrderRepository extends CrudRepository<OrderEntity, Integer> {
 
-    
+    @Query("SELECT o FROM OrderEntity o WHERE o.clientEntity= ?1")
+    List<OrderEntity> findOrderByIdClient(ClientEntity clientEntity);
 }
